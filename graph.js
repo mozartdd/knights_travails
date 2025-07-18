7f6b56da2e498356c96e098b62a736abd32a55d9
Dainis Dilevka <thedenisovan@proton.me>
2025-07-18 15:45:10 +0100

    Fixed typo

--- graph.js
+++ graph.js
@@ -1,15 +1,26 @@
 function knightMoves(start, end) {
   if (
-    start[0] < 0 || start[0] > 7 ||
-    start[1] < 0 || start[1] > 7 ||
-    end[0] < 0   || end[0] > 7 ||
-    end[1] < 0   || end[1] > 7
+    start[0] < 0 ||
+    start[0] > 7 ||
+    start[1] < 0 ||
+    start[1] > 7 ||
+    end[0] < 0 ||
+    end[0] > 7 ||
+    end[1] < 0 ||
+    end[1] > 7
   ) {
     throw new Error('x and y coordinates must be integers between 0 and 7.');
   }
 
   let moves = [
-    [2, 1], [1, 2], [-1, 2], [-2, 1], [-2, -1], [-1, -2], [1, -2], [2, -1]
+    [2, 1],
+    [1, 2],
+    [-1, 2],
+    [-2, 1],
+    [-2, -1],
+    [-1, -2],
+    [1, -2],
+    [2, -1],
   ];
   let queue = [];
   let visited = new Set();
@@ -22,10 +33,9 @@ function knightMoves(start, end) {
     let [x, y] = position;
 
     // If reached end coordinates return path
-    if(x === end[0] && y === end[1]) 
-      {
-        return console.log(`=> You made it in ${path.length-1} moves!\n`, path);
-      }
+    if (x === end[0] && y === end[1]) {
+      return console.log(`=> You made it in ${path.length - 1} moves!\n`, path);
+    }
 
     for (let [moveX, moveY] of moves) {
       let newX = x + moveX;
@@ -41,4 +51,4 @@ function knightMoves(start, end) {
   }
 }
 
-knightMoves([0, 0], [7, 0]);
+knightMoves([0, 0], [3, 0]);
